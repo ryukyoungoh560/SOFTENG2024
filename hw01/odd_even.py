1040b9f01c7fc3a591e93a39cebf109c9fae196c
ryukyoungoh560 <93868737+ryukyoungoh560@users.noreply.github.com>
2024-09-11 10:21:48 +0900

    save

--- hw01/odd_even.py
+++ hw01/odd_even.py
@@ -2,10 +2,8 @@ def is_even(n):
     return n % 2 == 0
 
 
-
-
 def main():
-    n = int(input("정수를 입력하세요"))
+    n = int(input("숫자를 입력하세요"))
 
     if is_even(n) == True:
         print(f"숫자 {n}은 짝수입니다.")
