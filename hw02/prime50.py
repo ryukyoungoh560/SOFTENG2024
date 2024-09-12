872c7412e0d2afff5e3367bc08a26cb9485b0e84
ryukyoungoh560 <93868737+ryukyoungoh560@users.noreply.github.com>
2024-09-12 09:16:53 +0900

    rename

---
+++ hw02/prime50.py
@@ -0,0 +1,17 @@
+from prime_number import is_prime
+
+def main():
+    #primes = []
+    #for i in range(1,51):
+        #if is_prime(i) == True:
+            #primes.append()
+
+    primes = [i for i in range(2, 51) if is_prime(i)]
+    print(f"50까지의 소수: {primes}")
+    
+            
+
+
+
+if __name__ == "__main__":
+    main()
