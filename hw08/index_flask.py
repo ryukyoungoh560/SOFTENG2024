54ee276fc2aba329e0c8f6008c2e53cf878c519f
ryukyoungoh560 <93868737+ryukyoungoh560@users.noreply.github.com>
2024-10-19 23:52:29 +0900

    hw08d

--- hw08/index_flask.py
+++ hw08/index_flask.py
@@ -1,43 +1,22 @@
 from flask import Flask, render_template
 
-from datetime import datetime
+import os
 
 import pandas as pd 
 
 
 app = Flask(__name__)
- 
-posts = [
-    {
-        'author': {
-            'username': '오류경'
-        },
-        'title': 'first',
-        'content': '7번째 과제입니다',
-        'date_posted': datetime.strptime('2024-10-17', '%Y-%m-%d')
-    },
-    {
-        'author': {
-            'username': ''
-        },
-        'title': 'second',
-        'content': '7week',
-        'date_posted': datetime.strptime('2024-10-17', '%Y-%m-%d')
-    },
-]
-# df = pd.read_csv("data.csv")  csv파일 읽어서 자동완성(?)
-# post_list = df.to_dict(orient='records')
 
-for i,row in df.iterrows():
-    post.append({
-        "title": row['title'],
-        "content": row['content'] 
-    })
+csv_path = os.path.join(app.root_path, 'static', 'csv', 'data.csv') 
+
+df = pd.read_csv(csv_path) 
+post_list = df.to_dict(orient='records')
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title = 'Post', posts=posts)
+    return render_template('index.html', title = 'Post', posts=post_list)
 
 @app.route('/about')
 def about():
@@ -47,7 +26,7 @@ def about():
 def blog():
     return render_template('blog.html', title = 'Blog')
 
-app.run(debug=True)
 
-#if __name__ == '__main__':
-    #app.run(host="0.0.0.0", debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
