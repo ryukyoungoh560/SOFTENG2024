6140797b6aef76a364067a709ae0bcaa4e6ed78d
ryukyoungoh560 <93868737+ryukyoungoh560@users.noreply.github.com>
2024-10-25 22:58:39 +0900

    hw09

--- hw09/single_pages/views.py
+++ hw09/single_pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+import os
+import pandas as pd
+from django.conf import settings
 # Create your views here.
+
+
 
 def landing_page(request):
     return render(request, 'single_pages/landing.html', {'title':'home'})
@@ -9,10 +13,10 @@ def about_page(request):
     return render(request, 'single_pages/about_me.html', {'title':'About me'})
 
 def blog_list(request):
-    post_list=[
-        {
-            'title': 'first',
-            'content': 'test'
-        },
-    ]
+    csv_path = os.path.join(settings.BASE_DIR, "single_pages", "static", "single_pages", "csv", "data.csv")
+
+    df = pd.read_csv(csv_path)
+    post_list = df.to_dict(orient="records")
+
     return render(request, 'single_pages/blog.html', {'title':'Blog list', 'posts': post_list})
+
