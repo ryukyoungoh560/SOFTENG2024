34368b86892b9bc8bb93918e1ee9da9e32523ed7
ryukyoungoh560 <93868737+ryukyoungoh560@users.noreply.github.com>
2024-11-09 17:54:22 +0900

    dd

--- hw11/10_README.md
+++ hw11/10_README.md
@@ -9,3 +9,5 @@ ToDoList - 포스트
 ToDoItem - 댓글
 
 홈페이지 디자인에 head부분등에 녹여서 구현할 수 있음
+
+onclick 부분을 인식하지 못하는 문제발생
--- hw11/todo_app/views.py
+++ hw11/todo_app/views.py
@@ -8,11 +8,11 @@ from .models import ToDoList, ToDoItem
 
 class ListListView(ListView):
     model = ToDoList
-    template_name = "todo_app/index.html"
+    template_name = "index.html"
 
 class ItemListView(ListView):
     model = ToDoItem
-    template_name = "todo_app/todo_list.html"
+    template_name = "todo_list.html"
 
     def get_queryset(self):
         return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
