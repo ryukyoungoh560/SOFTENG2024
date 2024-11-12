793829a1283cf8dea8ab7def7be7bebb326d23d9
ryukyoungoh560 <93868737+ryukyoungoh560@users.noreply.github.com>
2024-11-12 11:44:04 +0900

    아아아ㅏ아아ㅏ아아아

--- hw12/single_pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "single_pages"
-
-urlpatterns = [
-    path('about_me/', views.about_page, name="about_me"),
-    path('', views.landing_page, name='landing_page'),
-]
