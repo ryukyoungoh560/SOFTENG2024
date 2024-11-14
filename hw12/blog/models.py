7af1c040b1a07a96821d68e51e8ea59805ff3eec
ryukyoungoh560 <93868737+ryukyoungoh560@users.noreply.github.com>
2024-11-14 16:04:54 +0900

    15p

--- hw12/blog/models.py
+++ hw12/blog/models.py
@@ -6,6 +6,9 @@ class Post(models.Model):
     title = models.CharField(max_length=100)    #제목 -캐릭터필드 최대 100글자
     content = models.TextField()                #제한없이 길게 쓸 수 있음
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank= True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank= True)
+
     created_at = models.DateTimeField(auto_now_add=True)    #저장 , auto~ -> 시간 자동완성?
     updated_at = models.DateTimeField(auto_now=True)
 
