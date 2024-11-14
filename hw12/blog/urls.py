02734f3046a91869ccaa21f102f237e507e64b83
ryukyoungoh560 <93868737+ryukyoungoh560@users.noreply.github.com>
2024-11-14 10:47:28 +0900

    dd

--- hw12/blog/urls.py
+++ hw12/blog/urls.py
@@ -5,7 +5,7 @@ app_name = "blog"
 
 urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view(), name= 'post_detail'),
-    path('', views.PostList.as_view(), name= 'post_list'),
+    path('blog/', views.PostList.as_view(), name= 'post_list'),
     path('about_me/', views.about_page, name="about_me"),
-    path('landing', views.landing_page, name='landing_page'),
+    path('', views.landing_page, name='landing_page'),
 ]
